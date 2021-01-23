468b955d90c675bc2b5105b787a8db4a76908569
fkanvaly <123azeDSQ>
2021-01-23 16:11:05 +0100

    before Node modif

--- graph.cpp
+++ graph.cpp
@@ -10,9 +10,9 @@ struct Node
 {
     /* data */
     int id;
-    unordered_map<int, Node* > neighboors;
-
     int degree=0;
+    unordered_map<int, bool > neighboors;
+
     
     Node():id(-1){}
 
@@ -20,7 +20,7 @@ struct Node
         :id(id_){};
 
     void add_edge(int dst_id, Node* dst_node){
-        neighboors[dst_id] = dst_node;
+        neighboors[dst_id] = true;
     }
 
     bool has_edge(int dst_id){
@@ -29,15 +29,28 @@ struct Node
     ~Node(){} 
 };
 
+struct Edge
+{
+    int src;
+    int dst;
+};
+
+
 struct Graph
 {
     /* data */
     public:
         unordered_map<int, Node*> nodes; 
+        unordered_map<int, unordered_map<int, bool> > edges; 
+        map<int, unordered_map<int, bool> > track;
         int n_edges=0;
         int lowest_degree=0;
 
     Graph(){
+    }
+
+    Graph copy(){
+        Graph g = Graph();
     }
 
     int nb_nodes(){
@@ -50,17 +63,56 @@ struct Graph
 
     void print_nodes(){
         for (auto &&node : nodes)
-            cout << node.second->id << endl;
+            cout << node.second->id << " degree: "<< node.second->degree << endl;
+    }
+
+    void print_degree(){
+        cout << "size: " << track.size() <<endl;
+        for (auto &&deg : track)
+        {
+            cout<< "degree : " << deg.first << endl;
+            for (auto &&node : deg.second)
+            {
+                cout<< "-> node: " << node.first << endl;
+            }
+        }
+        
+    }
+
+    void print_lowest_degree(){
+        cout<< "lowest degree : " << lowest_degree << endl;
+        if( track.find(lowest_degree) != track.end() ){
+            for (auto &&node : track[lowest_degree])
+            {
+                cout<< "-> node: " << node.first << endl;
+            }
+        }
+    }
+
+    int lowest_node(){
+        if( track.find(lowest_degree) != track.end() )
+            for (auto &&node : track[lowest_degree])
+                return node.first;
+    }
+
+    float average_degree_density(){
+        return n_edges/nodes.size();
     }
 
     void add_edge(int src, int dst){
+        // cout<<"src: "<<src<<" dst: "<<dst<<endl;
+        if(nodes.size()==0){
+            lowest_degree = 1;
+        }
         // create new node if they doesn't exist yet
         if(nodes.find(src)==nodes.end()){
             nodes[src] = new Node(src);
+            track[1][src] = true;
         }
 
         if(nodes.find(dst)==nodes.end()){
             nodes[dst] = new Node(dst);
+            track[1][dst] = true;
         }
 
         // if edge already exist
@@ -71,15 +123,69 @@ struct Graph
         Node* src_node = nodes[src];
         Node* dst_node = nodes[dst];
 
+        change_degree(src, src_node->degree+1);
+        change_degree(dst, dst_node->degree+1);
+
         // add connection in two directions
         src_node->add_edge(dst, dst_node);
         dst_node->add_edge(src, src_node);
 
-        // // change degree node
+        // change degree node
         n_edges++;
+
     }
 
+    void change_degree(int id, int deg){
+        Node* node = nodes[id];
+        int old_deg = node->degree;
+        // move the neigboor to new degree 
+        if (deg==old_deg)
+            return;        
 
+        track[old_deg].erase(id);
+        node->degree = deg;
+        track[deg][id] = true;
+        // cout << "node: "<< id << " from: " << old_deg << " to: " << deg<<endl;
+
+        if(lowest_degree>deg)
+            lowest_degree = deg;
+
+        if(track[old_deg].size()==0)
+            track.erase(old_deg);
+
+    }
+
+    void remove_node(int id, ){
+        Node* node = nodes[id];
+
+        // remove the node from its neighboors neighborhood
+        for(auto& elt: node->neighboors){
+            // remove the node in it's neigborhood
+            int n_id = elt.first;
+            Node* n_node = nodes[n_id];
+            n_node->neighboors.erase(id);
+
+            // move the neigboor to new degree
+            int n_deg = n_node->degree;
+            change_degree(n_id, n_deg-1) ;
+            n_edges--;
+            // cout << "neigh: "<< n_id << " from: " << n_deg << " to: " << n_node->degree<<endl;
+        }
+
+        // remove from degree tracker
+        if(track[node->degree].size()==1){
+            track.erase(node->degree);
+            if(node->degree == lowest_degree)
+                lowest_degree = track.begin()->first;
+        }
+        else
+        {
+            track[node->degree].erase(id);
+        }
+        
+        // remove from nodes
+        nodes.erase(id);
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include "graph.cpp"
+#include <unordered_map> 
+#include <fstream>
+#include <vector> 
 
 using namespace std;
 
@@ -25,22 +28,75 @@ Graph read_graph(string filename){
     return G;
 }
 
+unordered_map<int, bool> densest_graph(Graph G){
+
+    // init H
+    unordered_map<int, bool> H;
+    float H_density = G.average_degree_density();
+    for (auto &&node : G.nodes)
+        H[node.first] = true;
+
+    vector<int> node_to_delete_in_H;
+    // while (G contains at least one edge)
+    int i_max = G.nodes.size();
+    int i=0;
+    while (G.n_edges>0 && i<i_max+1)
+    {
+        G.print_lowest_degree();
+        int node_id = G.lowest_node();
+        cout << "node_to_remove: "<<node_id<< endl;
+        G.remove_node(node_id);
+
+        node_to_delete_in_H.push_back(node_id);
+        if(G.average_degree_density() > H_density){
+            for (auto &&id : node_to_delete_in_H)
+                H.erase(id);
+            node_to_delete_in_H.clear();            
+        }
+        i++;
+    }
+
+    return H;
+}
+
+
 int main()
 {
-    // graph_t G = read_graph("../data/facebook_combined.txt");
-    // vector<int> nodes = G.get_nodes();
-    // cout<<"nb of edge: "<<G.nb_edges()<<endl;
-    // cout<<"nb of nodes: "<<G.nb_nodes()<<endl;
-
-    Graph G = Graph();
-
-    G.add_edge(1,2);
-    G.add_edge(2,3);
-    G.add_edge(1,4);
-    G.add_edge(4,2);
-
-    G.print_nodes();
+    Graph G = read_graph("../data/facebook_combined.txt");
+    // Graph G = read_graph("../data/com-dblp.ungraph.txt");
     
+    cout<<"nb of edge: "<<G.n_edges<<endl;
+    cout<<"nb of nodes: "<<G.nodes.size()<<endl;
 
+    // EXAMPLE
+    // Graph G = Graph();
+    // G.add_edge(8, 4);
+    // G.add_edge(9, 4);
+    // G.add_edge(10, 4);
+    // G.add_edge(4, 7);
+    // G.add_edge(4, 2);
+    // G.add_edge(7, 5);
+    // G.add_edge(5, 1);
+    // G.add_edge(1, 3);
+    // G.add_edge(3, 6);
+
+    // G.add_edge(6, 2);
+    // G.add_edge(2, 5);
+
+    // G.add_edge(2, 3);
+    // G.add_edge(2, 1);
+    // G.add_edge(5, 6);
+    // G.add_edge(1, 6);
+    // G.add_edge(5, 3);
+
+    unordered_map<int, bool> H = densest_graph(G);
+
+    ofstream myfile;
+    myfile.open ("densest.txt");
+    for (auto &&node : H){
+        cout<<node.first<<endl;
+        myfile << node.first << "\n";
+    }
+    myfile.close();
 
 }
